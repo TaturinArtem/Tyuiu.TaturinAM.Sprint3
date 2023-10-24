361955ab7a5dd49f7a4117dc00693489f3899422
TaturinArtem <144136367+TaturinArtem@users.noreply.github.com>
2023-10-24 16:47:53 +0500

    Task4
    Task4

---
+++ Tyuiu.TaturinAM.Sprint3.Task4.V14.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.TaturinAM.Sprint3.Task4.V14.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.TaturinAM.Sprint3.Task4.V14.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2971c259-ca2f-4a26-b850-ee9aa888f79f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
